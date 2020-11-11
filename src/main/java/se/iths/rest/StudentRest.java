61319836b26dfad56bc38fe7bbc5e76c3152c496
Patrik Gustafsson <54358596+PG-87@users.noreply.github.com>
2020-11-11 15:01:50 +0100

    first commit

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,81 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("create")
+    @POST
+    public Response createNewStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudentById(@PathParam("id") Long id) {
+        Student foundStudent = studentService.findStudentById(id);
+        if (foundStudent != null){
+            return Response.ok(foundStudent).build();
+        } else {
+            throw new StudentNotFoundException("Cant find the student");
+        }
+    }
+
+    @Path("getByLastName/{lastName}")
+    @GET
+    public List<Student> getStudentByLastName(@PathParam("lastName") String lastName) {
+        List<Student> foundStudents = studentService.findStudentByLastName(lastName);
+        if (!foundStudents.isEmpty()){
+            return studentService.findStudentByLastName(lastName);
+        } else {
+            throw new StudentNotFoundException("No student with that last name exist in database");
+        }
+    }
+
+    @Path("getAllStudents")
+    @GET
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateStudent(Student student) {
+        Student updatedStudent = studentService.updateStudent(student);
+        if (updatedStudent != null) {
+            return Response
+                   .ok(updatedStudent)
+                   .build();
+        } else {
+            throw new StudentNotFoundException("Cant find student to update!");
+        }
+    }
+
+    @Path("removeStudent/{id}")
+    @DELETE
+    public Response removeStudent(@PathParam("id") Long id){
+        Student foundStudent = studentService.findStudentById(id);
+        if (foundStudent != null){
+            studentService.removeStudentById(id);
+            return Response.ok()
+                    .entity("Student with Id: " + id + " was successfully deleted!")
+                    .build();
+        } else {
+            throw new StudentNotFoundException("Wrong Id!");
+        }
+    }
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,13 +1,16 @@
 package se.iths.service;
 
 import se.iths.entity.Student;
+
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
-
+@Transactional
 public class StudentService {
 
-
+    @PersistenceContext
     EntityManager entityManager;
 
     public Student createStudent(Student student) {
@@ -15,7 +18,8 @@ public class StudentService {
         return student;
     }
 
-    public Student updateTodo(Student student) {
+    public Student updateStudent(Student student) {
+        if (student != null)
         entityManager.merge(student);
         return student;
     }
@@ -24,8 +28,17 @@ public class StudentService {
         return entityManager.find(Student.class, id);
     }
 
-    public List<Student> getAllStudents() {
-        return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
+    public List<Student> findStudentByLastName(String lastName) {
+        return entityManager.
+                createQuery("SELECT p FROM Student p WHERE p.lastName = \'" + lastName + "\'", Student.class)
+                .getResultList();
     }
 
+    public List<Student> getAllStudents(){
+        return entityManager.createQuery("SELECT i from Student i", Student.class).getResultList();
+    }
+
+    public void removeStudentById(Long id) {
+        entityManager.remove(findStudentById(id));
+    }
 }
